e6b9c22316ae802ea7b51c00bd47e792324e3ecc
PurplMochii <98187845+ghastlytim@users.noreply.github.com>
2023-06-24 23:53:58 -0700

    Update savemanager.lua

--- savemanager.lua
+++ savemanager.lua
@@ -1,7 +1,7 @@
 local httpService = game:GetService('HttpService')
 
 local SaveManager = {} do
-	SaveManager.Folder = 'SomaHook'
+	SaveManager.Folder = 'Habit'
 	SaveManager.Ignore = {}
 	SaveManager.Parser = {
 		Toggle = {
